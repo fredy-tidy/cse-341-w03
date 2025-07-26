f8a61203f23199987a12cace876ac47cde89739a
fredy-tidy <fredy.castaneda.s21@gmail.com>
2025-07-26 13:43:54 -0600

    server runnig with api two collections

--- routes/courses.js
+++ routes/courses.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const coursesController = require('../controller/students');
+const coursesController = require('../controller/students'  );
 
 router.get('/students/getAll', coursesController.getAll);
 router.get('/students/getOne/:id', coursesController.getSingle);
@@ -12,8 +12,16 @@ router.use('/',require('./swagger'));
 
 router.get('/', (req,res) => {
     // swagger.tags=['Hello World']
-    res.send('Hellow World');
+    res.send('Hellow World at week 03');
 });
+
+const classesController = require('../controller/classes');
+router.get('/classes/getAllClasses', classesController.getAllClasses);
+router.get('/classes/getSingleClass/:id', classesController.getSingleClass);
+router.post('/classes/createClass', classesController.createClass);
+router.put('/classes/updateClass/:id', classesController.updateClass);
+router.delete('/classes/deleteClass/:id', classesController.deleteClass);
+
 
 
 module.exports = router;
